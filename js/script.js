46a61b7475588fcf210bf3b722435e0e0a5f2226
Bayezid Billah <bayezidbillah@gmail.com>
2021-12-31 12:41:53 +0600

    31st

--- js/script.js
+++ js/script.js
@@ -5,31 +5,32 @@ $(function() {
             $("#collapsable-nav").collapse('hide');
         }
     });
-
-
-    $("#navbarTogggle").click(function(event) {
-        $(event.target).focus();
-    });
 });
 
+(function(global) {
+    var bb = {};
+    var homeHtml = "snippets/home-snippet.html";
+    var insertHtml = function(selector, html) {
+        var targetElem = document.querySelector(selector);
+        targetElem.innerHtml = html;
+    };
 
-// $(function() { // Same as document.addEventListener("DOMContentLoaded"...
+    // Show loadin icon inside element identified by 'selector'.'
+    var showLoading = function(selector) {
+        var html = "<div class='text-center'>";
+        html += "<img src='images/ajax-loader.gif'></div";
+        insertHtml(selector, html);
+    };
 
-//     // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
-//     $("#navbarToggle").blur(function(event) {
-//         var screenWidth = window.innerWidth;
-//         if (screenWidth < 768) {
-//             $("#collapsable-nav").collapse('hide');
-//         }
-//     });
-
-//     // In Firefox and Safari, the click event doesn't retain the focus
-//     // on the clicked button. Therefore, the blur event will not fire on
-//     // user clicking somewhere else in the page and the blur event handler
-//     // which is set up above will not be called.
-//     // Refer to issue #28 in the repo.
-//     // Solution: force focus on the element that the click event fired on
-//     $("#navbarToggle").click(function(event) {
-//         $(event.target).focus();
-//     });
-// });
+    //On page Load (before images or CSS)
+    document.addEventListener("DOMContentLoaded", function(event) {
+        //on first load, show home view
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(
+            homeHtml,
+            function(responseText) {
+                document.querySelector("#main-content").innerHTML = responseText;
+            }, false);
+    });
+    global.$dc = dc;
+})(window);
